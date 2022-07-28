0851f87cc691566e3394b617b123b8da286a5ea3
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-28 08:25:48 -0500

    test mods

--- events/views.py
+++ events/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'events/home.html', {})
+def home(request, month, year):
+    name = "Stephen"
+    return render(request, 'events/home.html', {
+        "name": name,
+        "month": month,
+        "year": year,
+    })
