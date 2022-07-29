349e27ec042dcacaa5fd1002bcbd33d1f659cea1
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-29 08:06:12 -0500

    update home fxn

--- events/views.py
+++ events/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 import calendar
-from calendar import HTMLCalender
+from calendar import HTMLCalendar
 
 # Create your views here.
 
 
 def home(request, month, year):
     name = "Stephen"
+    # Convert month from name to number
+    month_number = list(calendar.month_name).index(month.title())
+    month_number = int(month_number)
     return render(request, 'events/home.html', {
         "name": name,
         "month": month,
         "year": year,
+        "month_number": month_number,
     })
