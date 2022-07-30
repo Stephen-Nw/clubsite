d20a8364304309dcc23b63a456720a196e687f7d
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-29 21:12:24 -0500

    add current year

--- events/views.py
+++ events/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
-
-# Create your views here.
+from datetime import datetime
 
 
 def home(request, month, year):
     name = "Stephen"
+
     # Convert month from name to number
     month_number = list(calendar.month_name).index(month.title())
     month_number = int(month_number)
-    # Create a calendar
-    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # Create a calendar... default calendar starts on Monday (firstweekday=0) so this has to be changed to Sunday (firstweekday=6)
+    cal = HTMLCalendar(firstweekday=6).formatmonth(year, month_number)
+
+    # Get current year
+    now = datetime.now()
+    current_year = now.year
+
     return render(request, 'events/home.html', {
         "name": name,
         "month": month,
         "year": year,
         "month_number": month_number,
         "cal": cal,
+        "current_year": current_year,
     })
