e7e139b1c6298e7393320ff37f996d5444ca6ae0
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-30 10:34:39 -0500

    register models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event, Venue, MyClubUser
 
 # Register your models here.
+# Models ar registered on this page so that they show up in the admin area
+
+admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
