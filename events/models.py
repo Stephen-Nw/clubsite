85b4e5eeec55c07ff221961c8c32a2aad581fe7c
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-30 11:07:28 -0500

    remove db_column from model fields

--- events/models.py
+++ events/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Venue(models.Model):
-    name = models.CharField(db_column='Venue Name', max_length=120)
+    name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
-    zip_code = models.CharField(db_column='Zip Code', max_length=30)
-    phone = models.CharField(db_column='Contact Phone', max_length=30)
-    web = models.URLField(db_column='Website Address')
-    email_address = models.EmailField(db_column='Email Address')
+    zip_code = models.CharField('Zip Code', max_length=30)
+    phone = models.CharField('Contact Phone', max_length=30)
+    web = models.URLField('Website Address')
+    email_address = models.EmailField('Email Address')
 
     def __str__(self):
         return self.name
@@ -18,15 +18,15 @@ class Venue(models.Model):
 class MyClubUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(db_column='User Email')
+    email = models.EmailField('User Email')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
 
 class Event(models.Model):
-    name = models.CharField(db_column='Event Name', max_length=120)
-    event_date = models.DateTimeField(db_column='Event Date')
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
     # venue = models.CharField(max_length=120)
     # Use ForeignKey in many-to-one relationships; do not use in many-to-many relationships
     venue = models.ForeignKey(
