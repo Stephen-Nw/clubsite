a4112024e04952f7dc338cd1d1464ee63c10bf99
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-03 08:55:27 -0500

    update

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import Event, Venue, MyClubUser
 
-# Register your models here.
-# Models ar registered on this page so that they show up in the admin area
 
-admin.site.register(Venue)
+# Models are registered on this page so that they show up in the admin area
+# admin.site.register(Venue)
+# admin.site.register(MyClubUser)
+# admin.site.register(Event)
+
+
+# admin.site.register(Venue)
 admin.site.register(MyClubUser)
-admin.site.register(Event)
+# admin.site.register(Event)
+
+# CUSTOMIZE THE MODEL DISPLAY IN ADMIN
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    # Selects column from model to display
+    list_display = ('name', 'address', 'phone')
+    # Which column should be used to sort; don't forget comma
+    ordering = ('name',)
+    # Add a search bar as well as search parameters
+    search_fields = ('name', 'address')
+
+
+# @admin.register(Event)
+# class EventAdmin(admin.ModelAdmin):
+#     fields =
