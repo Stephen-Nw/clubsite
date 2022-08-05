e8978c0a551246d3121649250b5d15434823e110
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-04 22:12:06 -0500

    create show_venue page

--- events/views.py
+++ events/views.py
@@ -7,6 +7,11 @@ from .models import Event, Venue
 from .forms import VenueForm
 
 
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'events/show_venue.html', {'venue': venue})
+
+
 def list_venues(request):
     venue_list = Venue.objects.all()
     return render(request, 'events/venue.html', {"venue_list": venue_list})
