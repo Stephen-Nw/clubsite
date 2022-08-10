34537c3c51e5cb3127d5f96833556683b81e7ea0
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-09 21:46:11 -0500

    add delete venue logic

--- events/views.py
+++ events/views.py
@@ -7,6 +7,12 @@ from .models import Event, Venue
 from .forms import VenueForm, EventForm
 
 
+def delete_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    venue.delete()
+    return redirect('list-venues')
+
+
 def delete_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     event.delete()
