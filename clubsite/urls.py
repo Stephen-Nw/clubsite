c1e91dedcdedd55386745cce3458d3f49c0cf3d9
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-12 11:00:03 -0500

    add django authentication

--- clubsite/urls.py
+++ clubsite/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    # Django authentication system
+    path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 ]
 
