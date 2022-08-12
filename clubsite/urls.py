d67155dc114b68b142fa8b21185f7f50118ea628
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-12 10:57:41 -0500

    add members path to project

--- clubsite/urls.py
+++ clubsite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('members.urls')),
 ]
 
 
