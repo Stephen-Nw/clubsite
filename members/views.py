35ba5f8e4d838060698e64ce4e5bde002ed2499c
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-12 11:46:02 -0500

    add login logic

--- members/views.py
+++ members/views.py
@@ -6,4 +6,18 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            # Return an 'invalid login' error message.
+            messages.success(
+                request, message="There was an error logging in. Try again...")
+            return redirect('login')
+
+    else:
+        return render(request, 'authenticate/login.html', {})
