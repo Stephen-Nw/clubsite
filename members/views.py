2f7f3196f78636a2b7eaf656cb595a32db9be288
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-16 07:56:06 -0500

    validate user registration info, save, and login

--- members/views.py
+++ members/views.py
@@ -29,4 +29,12 @@ def logout_user(request):
 
 
 def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
     return render(request, 'authenticate/register_user.html', {})
