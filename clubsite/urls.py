211dc349d4fbb2fdb096388ac744df9cfaac3a4f
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-27 17:10:40 -0500

    add media to url pattern

--- clubsite/urls.py
+++ clubsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,7 +24,7 @@ urlpatterns = [
     # Django authentication system
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # Configure Admin Titles
