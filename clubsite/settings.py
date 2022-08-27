4fb65ddbd51b513ea5578ab53ecadc36535bfe74
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-08-27 17:04:20 -0500

    add static root

--- clubsite/settings.py
+++ clubsite/settings.py
@@ -122,6 +122,13 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# *****************************************************************************
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# *****************************************************************************
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
