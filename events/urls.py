9f09ee9cb3e8e620335ad6d4657fca85cf1324b8
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-01 22:29:00 -0500

    create show_event url

--- events/urls.py
+++ events/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('search_events', views.search_events, name="search_events"),
     path('admin_approval', views.admin_approval, name="admin_approval"),
     path('venue_events/<venue_id>', views.venue_events, name='venue-events'),
+    path('show_event/<event_id>', views.show_event, name='show-event'),
 
 
 
